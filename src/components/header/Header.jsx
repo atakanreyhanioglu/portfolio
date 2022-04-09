56cea7f44c072338fe993ea1098a80132a4ba099
atakanreyhanioglu <atakan.reyhanioglu@gmail.com>
2022-04-09 16:54:47 +0300

    nav completed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
             <div className="container header__container">
                 <h5>Hello I'm</h5>
                 <h1>Atakan Reyhanioğlu</h1>
-                <h5 className="text-light">Fullstack Developer</h5>
+                <h5 className="text-light"><b>Curious</b> Fullstack Developer</h5>
                 <CTA/>
                 <Socials/>
                 <div className="me">
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import "./nav.css"
 import {BiHomeSmile, BiUser, BiBookReader, BiCodeBlock, BiMessageSquareEdit} from 'react-icons/bi'
+import {useState} from "react";
 
 const Nav = () => {
+    const [activeNav, setActiveNav] = useState('#home')
     return (
         <nav>
-            <a href="#home"><BiHomeSmile/></a>
-            <a href="#about"><BiUser/></a>
-            <a href="#experience"><BiBookReader/></a>
-            <a href="#services"><BiCodeBlock/></a>
-            <a href="#contact"><BiMessageSquareEdit/></a>
+            <a href="#home" onClick={() => setActiveNav('#home')} className={activeNav === '#home' ? 'active' : ''}><BiHomeSmile/></a>
+            <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BiUser/></a>
+            <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBookReader/></a>
+            <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><BiCodeBlock/></a>
+            <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareEdit/></a>
         </nav>
     )
 }
