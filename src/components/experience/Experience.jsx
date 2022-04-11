d3fe8f61d726eeddb83a3a682b81307c69cb3b4a
atakanreyhanioglu <atakan.reyhanioglu@gmail.com>
2022-04-11 11:04:13 +0300

    badges completed

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -33,35 +33,35 @@ const Experience = () => {
                     <h3>Languages</h3>
                     <div className="experience__content">
                         <article className="experience__details">
-                            <FaNodeJs/>
+                            <FaNodeJs className="experience-icon"/>
                             <div>
                                 <h4>Node.JS</h4>
                                 <small className="text-light">Experienced</small>
                             </div>
                         </article>
                         <article className="experience__details">
-                            <SiJavascript/>
+                            <SiJavascript className="experience-icon"/>
                             <div>
                                 <h4>JavaScript</h4>
                                 <small className="text-light">Experienced</small>
                             </div>
                         </article>
                         <article className="experience__details">
-                            <SiTypescript/>
+                            <SiTypescript className="experience-icon"/>
                             <div>
                                 <h4>TypeScript</h4>
                                 <small className="text-light">Experienced</small>
                             </div>
                         </article>
                         <article className="experience__details">
-                            <SiHtml5/>
+                            <SiHtml5 className="experience-icon"/>
                             <div>
                                 <h4>HTML5</h4>
                                 <small className="text-light">Experienced</small>
                             </div>
                         </article>
                         <article className="experience__details">
-                            <SiCss3/>
+                            <SiCss3 className="experience-icon"/>
                             <div>
                                 <h4>CSS3</h4>
                                 <small className="text-light">Experienced</small>
@@ -73,35 +73,35 @@ const Experience = () => {
                     <h3>Frameworks</h3>
                     <div className="experience__content">
                         <article className="experience__details">
-                            <SiNestjs/>
+                            <SiNestjs className="experience-icon"/>
                             <div>
                                 <h4>NestJS</h4>
                                 <small className="text-light">Intermediate</small>
                             </div>
                         </article>
                         <article className="experience__details">
-                            <SiExpress/>
+                            <SiExpress className="experience-icon"/>
                             <div>
                                 <h4>Express</h4>
                                 <small className="text-light">Experienced</small>
                             </div>
                         </article>
                         <article className="experience__details">
-                            <SiVuedotjs/>
+                            <SiVuedotjs className="experience-icon"/>
                             <div>
                                 <h4>Vue.js</h4>
                                 <small className="text-light">Experienced</small>
                             </div>
                         </article>
                         <article className="experience__details">
-                            <SiAngular/>
+                            <SiAngular className="experience-icon"/>
                             <div>
                                 <h4>Angular</h4>
                                 <small className="text-light">Intermediate</small>
                             </div>
                         </article>
                         <article className="experience__details">
-                            <SiNuxtdotjs/>
+                            <SiNuxtdotjs className="experience-icon"/>
                             <div>
                                 <h4>Nuxt.js</h4>
                                 <small className="text-light">Experienced</small>
@@ -113,28 +113,28 @@ const Experience = () => {
                     <h3>Databases</h3>
                     <div className="experience__content">
                         <article className="experience__details">
-                            <SiMongodb/>
+                            <SiMongodb className="experience-icon"/>
                             <div>
                                 <h4>MongoDB</h4>
                                 <small className="text-light">Experienced</small>
                             </div>
                         </article>
                         <article className="experience__details">
-                            <SiPostgresql/>
+                            <SiPostgresql className="experience-icon"/>
                             <div>
                                 <h4>Postgresql</h4>
                                 <small className="text-light">Experienced</small>
                             </div>
                         </article>
                         <article className="experience__details">
-                            <SiMysql/>
+                            <SiMysql className="experience-icon"/>
                             <div>
                                 <h4>Mysql</h4>
                                 <small className="text-light">Experienced</small>
                             </div>
                         </article>
                         <article className="experience__details">
-                            <SiRedis/>
+                            <SiRedis className="experience-icon"/>
                             <div>
                                 <h4>Redis</h4>
                                 <small className="text-light">Experienced</small>
@@ -146,42 +146,42 @@ const Experience = () => {
                     <h3>Hosting & Others</h3>
                     <div className="experience__content">
                         <article className="experience__details">
-                            <SiDocker/>
+                            <SiDocker className="experience-icon"/>
                             <div>
                                 <h4>Docker</h4>
                                 <small className="text-light">Experienced</small>
                             </div>
                         </article>
                         <article className="experience__details">
-                            <SiHeroku/>
+                            <SiHeroku className="experience-icon"/>
                             <div>
                                 <h4>Heroku</h4>
                                 <small className="text-light">Experienced</small>
                             </div>
                         </article>
                         <article className="experience__details">
-                            <SiVercel/>
+                            <SiVercel className="experience-icon"/>
                             <div>
                                 <h4>Vercel</h4>
                                 <small className="text-light">Experienced</small>
                             </div>
                         </article>
                         <article className="experience__details">
-                            <SiGooglecloud/>
+                            <SiGooglecloud className="experience-icon"/>
                             <div>
                                 <h4>GoogleCloud</h4>
                                 <small className="text-light">Intermediate</small>
                             </div>
                         </article>
                         <article className="experience__details">
-                            <SiFirebase/>
+                            <SiFirebase className="experience-icon"/>
                             <div>
                                 <h4>FireBase</h4>
                                 <small className="text-light">Experienced</small>
                             </div>
                         </article>
                         <article className="experience__details">
-                            <SiJira/>
+                            <SiJira className="experience-icon"/>
                             <div>
                                 <h4>Jira</h4>
                                 <small className="text-light">Experienced</small>
