1d42b5e4c00ff9e0a04fc84c261564f88eb55711
atakanreyhanioglu <atakan.reyhanioglu@gmail.com>
2022-04-11 12:56:02 +0300

    portfolio edited

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./portfolio.css"
 import tiwiserImg from '../../assets/panel-tiwiser.svg'
+import digibook from '../../assets/digibook.svg'
+import colorsNuxt from '../../assets/clors-nuxt.svg'
 
 const Portfolio = () => {
     //@TODO Live demo altına detayları ekle
@@ -10,11 +12,28 @@ const Portfolio = () => {
             <h2>Check The Source</h2>
             <div className="container portfolio__container">
                 <article className="portfolio__item">
-                    <div className="portfolio__item-image">
-                    <img src={tiwiserImg} alt="panel tiwiser live preview"/>
-                    </div>
-                    <h3>Tiwiser Panel</h3>
-                    <a href="https://panel.tiwiser.com/landing" rel="noreferrer" className="btn btn-primary" target="_blank">Live Demo</a>
+                    <a href="https://panel.tiwiser.com/landing" rel="noreferrer" target="_blank">
+                        <h4 className="title">Tiwiser Dashboard</h4>
+                        <div className="portfolio__item-image">
+                            <img src={tiwiserImg} alt=" tiwiser dashboard live preview"/>
+                        </div>
+                    </a>
+                </article>
+                <article className="portfolio__item">
+                    <a href="https://colors-nuxt.vercel.app" rel="noreferrer" target="_blank">
+                        <h4 className="title">Webcolors</h4>
+                        <div className="portfolio__item-image">
+                            <img src={colorsNuxt} alt="Colors for images live preview"/>
+                        </div>
+                    </a>
+                </article>
+                <article className="portfolio__item">
+                    <a href="https://panel.tiwiser.com/landing" rel="noreferrer" target="_blank">
+                        <h4 className="title">Tiwiser Digibook</h4>
+                        <div className="portfolio__item-image">
+                            <img src={digibook} alt="Digibook tiwiser live preview"/>
+                        </div>
+                    </a>
                 </article>
             </div>
         </section>
